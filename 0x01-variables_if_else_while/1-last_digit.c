e6e8086958e5c26ee8759a7d34e525ba202846eb
Kiren Srinivasan <srinitude@gmail.com>
2017-05-11 19:22:46 +0000

    Adds newline in 'print last digit' C file

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,22 +14,20 @@
 int main(void)
 {
 	int n;
-	int last_digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	last_digit = n % 10;
-	if (last_digit > 5)
+	if (n % 10 > 5)
 	{
-		printf("Last digit of %i is %i and is greater than 5", n, last_digit);
+		printf("Last digit of %i is %i and is greater than 5\n", n, n % 10);
 	}
-	else if (last_digit == 0)
+	else if (n % 10 == 0)
 	{
-		printf("Last digit of %i is %i and is 0", n, last_digit);
+		printf("Last digit of %i is %i and is 0\n", n, n % 10);
 	}
 	else
 	{
-		printf("Last digit of %i is %i and is less than 6 and not 0", n, last_digit);
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, n % 10);
 	}
 	return (0);
 }
