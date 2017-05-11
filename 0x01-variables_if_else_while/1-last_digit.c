fe3145f00c862795fe59a300dbb3bab18555e5e1
Kiren Srinivasan <srinitude@gmail.com>
2017-05-11 19:15:07 +0000

    Removes universal printf in 'get last digit' C file
    Changes the common part that was printed in a separate printf statement and puts them all in their own print statements depending on the value of the last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,18 +19,17 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	last_digit = n % 10;
-	printf("Last digit of %i is %i ", n, last_digit);
 	if (last_digit > 5)
 	{
-		printf("and is greater than 5");
+		printf("Last digit of %i is %i and is greater than 5", n, last_digit);
 	}
 	else if (last_digit == 0)
 	{
-		printf("and is 0");
+		printf("Last digit of %i is %i and is 0", n, last_digit);
 	}
 	else
 	{
-		printf("and is less than 6 and not 0");
+		printf("Last digit of %i is %i and is less than 6 and not 0", n, last_digit);
 	}
 	return (0);
 }
