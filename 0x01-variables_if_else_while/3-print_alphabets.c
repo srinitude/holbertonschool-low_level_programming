cce9c25d6bfce14258de60aae552ce863fcfb05c
Kiren Srinivasan <srinitude@gmail.com>
2017-05-11 20:01:31 +0000

    Adds initial 'print upper and lowercase alphabet' C file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description - Prints every lowercase letter in alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ\0";
+	int index = 0;
+
+	while (alphabet[index] != '\0')
+	{
+		putchar(alphabet[index]);
+		index++;
+	}
+	putchar('\n');
+	return (0);
+}
