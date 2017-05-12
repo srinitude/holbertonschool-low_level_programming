63e588cd0f24d4eb9e961951b7b8276ad749d114
Kiren Srinivasan <srinitude@gmail.com>
2017-05-12 02:13:15 +0000

    Fixes betty 'errors' in '3-digit unique combo' C file

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description - Print 3 consecutive digit trios of unique 0-9 combos 
+ * Description - Print 3 consecutive digit trios of unique 0-9 combos
  *
  * Return: Always 0 (Success)
  */
@@ -11,18 +11,18 @@
 int main(void)
 {
 	int first_number = '0';
-        int second_number = '1';
+	int second_number = '1';
 	int third_number = '2';
 
 	int original_third_start_point = '2';
-	
-	int running_second_start_point = '1';
-	int running_third_start_point = '2';	
 
-        while (first_number <= '7')
-        {
-                while (second_number <= '8')
-                {
+	int running_second_start_point = '1';
+	int running_third_start_point = '2';
+
+	while (first_number <= '7')
+	{
+		while (second_number <= '8')
+		{
 			while (third_number <= '9')
 			{
 				if ((first_number != second_number) != third_number)
@@ -48,12 +48,12 @@ int main(void)
 			}
 			second_number++;
 			third_number = running_third_start_point;
-                }
-                first_number++;
-                second_number = running_second_start_point;
+		}
+		first_number++;
+		second_number = running_second_start_point;
 		original_third_start_point++;
 		third_number = original_third_start_point;
-        }
-        putchar('\n');
-        return (0);
+	}
+	putchar('\n');
+	return (0);
 }
