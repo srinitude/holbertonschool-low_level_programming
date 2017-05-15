c0dba3ab48c5484c1db5cb754d0b7922870cdefd
Kiren Srinivasan <srinitude@gmail.com>
2017-05-15 22:57:56 +0000

    Adds initial 'print Holberton' C file

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char holberton = "Holberton\0";
+	int index = 0;
+	while (holberton[index] != 0)
+	{
+		_putchar(holberton[index]);
+		index++;
+	}
+	_putchar('\n');
+	return (0);
+}
