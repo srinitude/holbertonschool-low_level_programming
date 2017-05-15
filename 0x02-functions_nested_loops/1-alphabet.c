9d7b6a2d6df63b71900309431a09f1fd328aa619
Kiren Srinivasan <srinitude@gmail.com>
2017-05-15 23:35:09 +0000

    Removes main.c from Git tracking

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ *
+ * Return - Always Success (0)
+ */
+
+void print_alphabet(void)
+{
+	char letter = 'a';
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+}
