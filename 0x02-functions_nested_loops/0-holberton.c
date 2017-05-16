3d604394b2f3b4aaa3d3d63b22238ec86759ff35
Kiren Srinivasan <srinitude@gmail.com>
2017-05-16 00:14:07 +0000

    Changes typo in 'print Holberton' C file

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -11,7 +11,7 @@ int main(void)
 	char holberton[] = "Holberton\0";
 	int index = 0;
 
-	while (holberton[index] != 0)
+	while (holberton[index] != '\0')
 	{
 		_putchar(holberton[index]);
 		index++;
