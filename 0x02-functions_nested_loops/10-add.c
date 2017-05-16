7c2457a60e4f8c8d117df4cf7dd24e5880f7766c
Kiren Srinivasan <srinitude@gmail.com>
2017-05-16 19:30:29 +0000

    Adds initial 'return sum of 2 ints' C file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * add - Returns the sum of two integer
+ *
+ * @a: First operand
+ * @b: Second operand
+ *
+ * Return: An integer sum
+ *
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
