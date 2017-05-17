35c4a0d0dbb5340ff7c6af19157814cd7b8f7d72
Kiren Srinivasan <srinitude@gmail.com>
2017-05-17 01:10:51 +0000

    Adds initial 'print sum of 3/5 multiples up to 1024' C file

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int sum = 0;
+	int i;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 15 == 0)
+		{
+			sum += i;
+		}
+		else if (i % 3 == 0)
+		{
+			sum += i;
+		}
+		else if (i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+	printf("%i\n", sum);
+	return (0);
+}
