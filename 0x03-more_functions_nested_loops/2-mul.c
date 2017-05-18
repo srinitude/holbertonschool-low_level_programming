4ed318a53857dae6774e1901bc592b60baf5d012
Kiren Srinivasan <srinitude@gmail.com>
2017-05-18 19:48:32 +0000

    Adds initial 'returns the product of two integers' C file

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * mul - Returns the product of two integers
+ * @a: The first integer
+ * @b: The second integer
+ *
+ * Return: The product of two integers
+ *
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x03-more_functions_nested_loops/holberton.h
+++ 0x03-more_functions_nested_loops/holberton.h
@@ -13,3 +13,5 @@ void print_to_98(int n);
 void print_times_table(int n);
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
