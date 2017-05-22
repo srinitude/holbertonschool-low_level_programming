e4e76de286e0e1f6bca0a09a97ae1e9bd3ed8412
Kiren Srinivasan <srinitude@gmail.com>
2017-05-22 19:21:37 +0000

    Adds initial 'compute string length' C program

---
+++ 0x04-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+/**
+ * _strlen - Computes length of a string
+ * @s: The string in question
+ *
+ * Return: The length of said string
+ *
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s)
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
