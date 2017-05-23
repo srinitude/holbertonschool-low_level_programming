f95287248d9162bdeb1c86b010b29f53674c6b2a
Kiren Srinivasan <srinitude@gmail.com>
2017-05-23 18:47:42 +0000

    Adds extra logic to get correct output on 'reverse string' C program

--- 0x04-pointers_arrays_strings/5-rev_string.c
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
  * rev_string - Reverse a string
@@ -11,22 +12,36 @@ void rev_string(char *s)
 	char *beginning = s;
 	char old_letter = *s;
 	char new_letter;
+	int old_length = 0;
+	int length;
 
 	while (*s != '\0')
 	{
 		s++;
+		old_length++;
 	}
 	s--;
+	old_length--;
+	length = old_length;
 
 	new_letter = *s;
 
 	while (*beginning != '\0')
 	{
-		*s = new_letter;
-		*beginning = old_letter;
+		*beginning = new_letter;
+		*s = old_letter;
 		s--;
-		old_letter = *s;
 		beginning++;
-		new_letter = *beginning;
+		length--;
+		if (length >= (old_length / 2))
+		{
+			new_letter = *s;
+			old_letter = *beginning;
+		}
+		else
+		{
+			new_letter = *beginning;
+			old_letter = *s;
+		}
 	}
 }
