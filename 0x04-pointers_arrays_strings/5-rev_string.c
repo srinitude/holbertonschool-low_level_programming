63f87c0067ab67254f7766b4dda0530d5131e8c5
Kiren Srinivasan <srinitude@gmail.com>
2017-05-23 21:25:48 +0000

    Fixes logical errors in 'reverse string' C program

--- 0x04-pointers_arrays_strings/5-rev_string.c
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -1,47 +1,35 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
- * rev_string - Reverse a string
+ * rev_string - Reverses the string in place
  * @s: The string to reverse
  *
  */
 
 void rev_string(char *s)
 {
-	char *beginning = s;
-	char old_letter = *s;
-	char new_letter;
-	int old_length = 0;
-	int length;
+	char temp;
+	int length = 0;
+	int mid, i;
 
 	while (*s != '\0')
 	{
 		s++;
-		old_length++;
+		length++;
 	}
+	length--;
 	s--;
-	old_length--;
-	length = old_length;
+	mid = length / 2;
 
-	new_letter = *s;
-
-	while (*beginning != '\0')
+	for (i = 0; i < length; i++)
 	{
-		*beginning = new_letter;
-		*s = old_letter;
 		s--;
-		beginning++;
-		length--;
-		if (length >= (old_length / 2))
-		{
-			new_letter = *s;
-			old_letter = *beginning;
-		}
-		else
-		{
-			new_letter = *beginning;
-			old_letter = *s;
-		}
+	}
+	
+	for (i = 0; i <= mid; i++)
+	{
+		temp = s[i];
+		s[i] = s[length - i];
+		s[length - i] = temp;
 	}
 }
