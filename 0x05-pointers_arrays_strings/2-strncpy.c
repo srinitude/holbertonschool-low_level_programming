4565271f413a9d0310a9d336ff4c8c0df554254f
Kiren Srinivasan <srinitude@gmail.com>
2017-05-25 19:11:42 +0000

    Adds conditional in 'string copy' C program

--- 0x05-pointers_arrays_strings/2-strncpy.c
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -23,11 +23,10 @@ char *_strncpy(char *dest, char *src, int n)
 			n--;
 		}
 	}
-	
-	while (i < original_n)
+
+	if (i + 1 < original_n)
 	{
-		i++;
+		dest[i] = '\0';
 	}
-	dest[i] = '\0';
 	return (dest);
 }
