eb0415fbb364dabe209a1c078d55acc220e920c1
Kiren Srinivasan <srinitude@gmail.com>
2017-05-25 23:35:20 +0000

    Adds initial 'rot13 encoder' C program

---
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * rot13 - Transforms string with rot13 encoding
+ * @s: The string in question
+ *
+ * Return: A pointer to transformed string
+ *
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char r[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = r[j];
+			}
+		}
+	}
+	return (s);
+}
