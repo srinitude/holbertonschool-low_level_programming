e3080fba06c8b7ed14bc602a8c9ace96e78ba215
Kiren Srinivasan <srinitude@gmail.com>
2017-05-30 19:22:37 +0000

    Changes deference/parentheses syntax to bracket syntax in 'strchr' C program

---
+++ 0x06-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * _strspn - # of contiguous bytes in a string containing certain characters
+ * @s: The string in question
+ * @accept: The characters in question
+ *
+ * Return: The number of bytes
+ *
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int assigned = 0;
+	unsigned int count = 0;
+	int i, j;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				count++;
+				assigned = 1;
+			}
+		}
+		if (!assigned)
+			return (count);
+		assigned = 0;
+	}
+
+	return (count);
+}
