69b77106e3c4c72fcc45760cad93bb3e17c44ce3
Kiren Srinivasan <srinitude@gmail.com>
2017-05-30 20:25:39 +0000

    Adds initial 'strpbrk' C program

---
+++ 0x06-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _strpbrk - First occurrence of one of a set of bytes in a string
+ * @s: The string in question
+ * @accept: The set of chars to look through
+ *
+ * Return: Pointer to first occurrence
+ *
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				return (s + i);
+		}
+	}
+
+	return (0);
+}
