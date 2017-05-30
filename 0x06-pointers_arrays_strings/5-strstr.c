72567eb05d7a41020af62f81ccf78ea648ad91c4
Kiren Srinivasan <srinitude@gmail.com>
2017-05-30 20:48:35 +0000

    Adds initial 'strstr' C program

---
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+
+/**
+ * _strstr - Finds substring in a string
+ * @haystack: The string to search through
+ * @needle: The substring to find
+ *
+ * Return: Pointer to subtring first occurrence
+ *
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int needle_length = 0;
+	int matching_chars = 0;
+	int i, j, offset;
+
+	for (i = 0; *(needle + i) != '\0'; i++)
+		needle_length++;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i] != needle[j])
+			{
+				matching_chars = 0;
+				break;
+			}
+			else
+			{
+				if (!matching_chars)
+					offset = i;
+				matching_chars++;
+				i++;
+			}
+
+			if (needle_length == matching_chars)
+				return (haystack + offset);
+		}
+	}
+
+	return (0);
+}
