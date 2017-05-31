09f4576ccbd3574161ae84eae1c2969bc2e96d11
Kiren Srinivasan <srinitude@gmail.com>
2017-05-31 01:04:30 +0000

    Needle needs to be shorter than haystack

--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -12,11 +12,16 @@
 char *_strstr(char *haystack, char *needle)
 {
 	int needle_length = 0;
+	int haystack_length = 0;
 	int matching_chars = 0;
 	int i, j, offset;
 
-	for (i = 0; *(needle + i) != '\0'; i++)
+	for (i = 0; needle[i] != '\0'; i++)
 		needle_length++;
+	for (i = 0; haystack[i] != '\0'; i++)
+		haystack_length++;
+	if (needle_length > haystack_length)
+		return (0);
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
