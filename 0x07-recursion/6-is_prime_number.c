e91b72f6db68ba8b36e633b6de46b6b62dd9dfda
Kiren Srinivasan <srinitude@gmail.com>
2017-06-01 23:27:45 +0000

    Adds initial 'is_prime_number' C program

---
+++ 0x07-recursion/6-is_prime_number.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * prime_number_calc - Machinery behind is_prime_number
+ * @n: Number in question
+ * @divisor: Dvisior
+ *
+ * Return: 0 if prime, original # if not
+ */
+
+int prime_number_calc(int n, int divisor)
+{
+	if (n % divisor == 0)
+		return (n);
+	if (n == 1)
+		return (n);
+	if (divisor < (n / 2))
+		divisor += 1;
+	return (0 + prime_number_calc(n, divisor));
+}
+
+/**
+ * is_prime_number - Recursively calculate if number is prime
+ * @n: The number in question
+ *
+ * Return: 0 for false, 1 for true
+ *
+ */
+
+int is_prime_number(int n)
+{
+	int is_not_prime = prime_number_calc(n, 2);
+
+	if (is_not_prime)
+		return (0);
+	return (1);
+}
