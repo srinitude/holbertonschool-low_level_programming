3a04c35de11d81c795f8ba75f62048653a0512a7
Kiren Srinivasan <srinitude@gmail.com>
2017-06-03 00:27:32 +0000

    Adds initial, incorrect 'wildcmp' C program

---
+++ 0x07-recursion/100-wildcmp.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+int wildcard(char *f, char *s, int fir_idx, int sec_idx)
+{
+        if ((f[fir_idx] != '\0') && (s[sec_idx] != '\0'))
+        {
+                if ((f[fir_idx] != s[sec_idx]) && (s[sec_idx] != '*'))
+                        return (0);
+                if ((fir_idx == sec_idx) && (f[fir_idx] == s[sec_idx]))
+                        return (wildcard(f, s, fir_idx + 1, sec_idx + 1));
+                if ((s[sec_idx] == '*') && (s[sec_idx + 1] != '*'))
+                {
+                        fir_idx = sec_idx + 1;
+                        return (wildcard(f, s, fir_idx, sec_idx + 1));
+                }
+        }
+        return (1);
+}
+
+/**
+ * wildcmp - Compares two strings & sees if identical
+ * @s1: First string
+ * @s2: Second string
+ *
+ * Return: 1 if identical, 0 if not
+ *
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	return (wildcard(s1, s2, 0, 0));
+}
+
