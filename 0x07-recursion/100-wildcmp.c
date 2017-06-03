66e880e50b833a7809dc74212b9cd281ea94adec
Kiren Srinivasan <srinitude@gmail.com>
2017-06-03 00:35:32 +0000

    Fixes betty errors in 'wildcmp' C program

--- 0x07-recursion/100-wildcmp.c
+++ 0x07-recursion/100-wildcmp.c
@@ -1,20 +1,30 @@
 #include "holberton.h"
 
+/**
+ * wildcard - Wildcard checker
+ * @f: First string
+ * @s: Second string
+ * @fir_idx: First index
+ * @sec_idx: Second index
+ *
+ * Return: 1 if matching, 0 if not
+ */
+
 int wildcard(char *f, char *s, int fir_idx, int sec_idx)
 {
-        if ((f[fir_idx] != '\0') && (s[sec_idx] != '\0'))
-        {
-                if ((f[fir_idx] != s[sec_idx]) && (s[sec_idx] != '*'))
-                        return (0);
-                if ((fir_idx == sec_idx) && (f[fir_idx] == s[sec_idx]))
-                        return (wildcard(f, s, fir_idx + 1, sec_idx + 1));
-                if ((s[sec_idx] == '*') && (s[sec_idx + 1] != '*'))
-                {
-                        fir_idx = sec_idx + 1;
-                        return (wildcard(f, s, fir_idx, sec_idx + 1));
-                }
-        }
-        return (1);
+	if ((f[fir_idx] != '\0') && (s[sec_idx] != '\0'))
+	{
+		if ((f[fir_idx] != s[sec_idx]) && (s[sec_idx] != '*'))
+			return (0);
+		if ((fir_idx == sec_idx) && (f[fir_idx] == s[sec_idx]))
+			return (wildcard(f, s, fir_idx + 1, sec_idx + 1));
+		if ((s[sec_idx] == '*') && (s[sec_idx + 1] != '*'))
+		{
+			fir_idx = sec_idx + 1;
+			return (wildcard(f, s, fir_idx, sec_idx + 1));
+		}
+	}
+	return (1);
 }
 
 /**
