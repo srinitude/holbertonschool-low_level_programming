d4d582103380d6a8cc4f7f0fa1ee3f90983cc835
Kiren Srinivasan <srinitude@gmail.com>
2017-06-05 23:11:16 +0000

    Adds initial 'print name of executable' C program

---
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ * @argc: Number of arguments in program
+ * @argv: The arguments passed to the program
+ *
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
