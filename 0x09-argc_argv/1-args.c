69821124a68a453808c01a17e4ff82457bb935b8
Kiren Srinivasan <srinitude@gmail.com>
2017-06-05 23:19:02 +0000

    Adds initial 'print argument count' C program

---
+++ 0x09-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for program
+ * @argc: Number of arguments passed
+ * @argv: The arguments passed
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%i\n", argc - 1);
+	return (0);
+}
