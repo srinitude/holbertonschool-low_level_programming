eb1ed3a23247f3ed7f37553ee155b7a97bd706d6
Kiren Srinivasan <srinitude@gmail.com>
2017-06-06 23:21:29 +0000

    Adds initial 'create 2d array' C program

---
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - Creates 2D int array
+ * @width: Width of the array
+ * @height: Height of the array
+ *
+ * Return: Pointer to the 2D array
+ *
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **arr;
+	int i, j, total;
+
+	if ((width < 1) || (height < 1))
+		return (NULL);
+	total = width * height;
+	arr = (int **)malloc(total * sizeof(int));
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			arr[i][j] = 0;
+	}
+	return (arr);
+}
