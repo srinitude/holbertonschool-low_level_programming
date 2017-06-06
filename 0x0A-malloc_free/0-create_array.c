4bee20c77901aa2a8f736f540058992855f4ef22
Kiren Srinivasan <srinitude@gmail.com>
2017-06-06 20:03:45 +0000

    Switches declaration order in 'create char arr on heap' C program

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -12,11 +12,11 @@
 
 char *create_array(unsigned int size, char c)
 {
-	if (size == 0)
-		return (NULL);
 	char *arr = malloc(sizeof(c) * size);
 	unsigned int i;
 
+	if (size == 0)
+		return (NULL);
 	for (i = 0; i < size; i++)
 	{
 		arr[i] = c;
