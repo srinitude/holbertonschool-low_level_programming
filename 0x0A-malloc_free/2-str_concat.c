b322014126add7ce875078b4f607dfd45505a37b
Kiren Srinivasan <srinitude@gmail.com>
2017-06-07 01:47:17 +0000

    Replaces null char with ptr to empty string in 'str concat' C program

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -14,12 +14,13 @@ char *str_concat(char *s1, char *s2)
 {
 	char *concat;
 	int i, j, total;
+	char *empty = "";
 	int s1_len, s2_len = 0;
 
 	if (s1 == NULL)
-		*s1 = '\0';
+		s1 = empty;
 	if (s2 == NULL)
-		*s2 = '\0';
+		s2 = empty;
 	for (i = 0; s1[i] != '\0'; i++)
 		s1_len++;
 	for (i = 0; s2[i] != '\0'; i++)
