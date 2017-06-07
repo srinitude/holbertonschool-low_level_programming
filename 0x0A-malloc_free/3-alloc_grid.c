e3ac57340c52eef551ac55b071dbcf5d56939264
Kiren Srinivasan <srinitude@gmail.com>
2017-06-07 02:19:41 +0000

    Sorts out malloc sizes and freeing memory in 'create grid' C program

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -17,11 +17,18 @@ int **alloc_grid(int width, int height)
 
 	if ((width < 1) || (height < 1))
 		return (NULL);
-	arr = (int **)malloc(width * sizeof(int));
-	for (i = 0; i < height; i++)
-		arr[i] = malloc(height * sizeof(int));
+	arr = malloc(height * sizeof(int *));
 	if (arr == NULL)
 		return (NULL);
+	for (i = 0; i < width; i++)
+	{
+		arr[i] = malloc(width * sizeof(int));
+		if (arr[i] == NULL)
+		{
+			free(arr);
+			return (NULL);
+		}
+	}
 	for (i = 0; i < height; i++)
 	{
 		for (j = 0; j < width; j++)
