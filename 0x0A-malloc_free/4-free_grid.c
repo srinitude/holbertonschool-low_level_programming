e43fe030bffd957623dfffd1c6d3d66656851b32
Kiren Srinivasan <srinitude@gmail.com>
2017-06-07 00:46:58 +0000

    Frees grid as a whole in addition to rows in 'free grid' C program

--- 0x0A-malloc_free/4-free_grid.c
+++ 0x0A-malloc_free/4-free_grid.c
@@ -12,8 +12,9 @@ void free_grid(int **grid, int height)
 {
 	int i;
 
-	if (grid == NULL || height == 0)
+	if ((grid == NULL) || (height == 0))
 		return;
 	for (i = 0; i < height; i++)
 		free(grid[i]);
+	free(grid);
 }
