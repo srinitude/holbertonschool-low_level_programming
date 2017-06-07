14dea8a4724018aa35bc083f419ea4b5e95a7319
Kiren Srinivasan <srinitude@gmail.com>
2017-06-07 19:01:47 +0000

    Adds null check in condition to copy in 'str concat' C program

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -29,10 +29,10 @@ char *str_concat(char *s1, char *s2)
 	concat = malloc(total * sizeof(char));
 	if (concat == NULL)
 		return (NULL);
-	for (i = 0; i < s1_len; i++)
+	for (i = 0; s1 && i < s1_len; i++)
 		concat[i] = s1[i];
 	j = i;
-	for (i = 0; i < s2_len; i++, j++)
+	for (i = 0; s2 && i < s2_len; i++, j++)
 		concat[j] = s2[i];
 	concat[j] = '\0';
 	return (concat);
