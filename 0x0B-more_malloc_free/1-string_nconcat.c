69b0176b79514e585f6cd37459911fabe0a8feca
Kiren Srinivasan <srinitude@gmail.com>
2017-06-08 19:34:15 +0000

    Adds case if NULL is passed in 'string_nconcat' C program

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -17,18 +17,18 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	int total, len = 0;
 	unsigned int i, j;
 
-	for (i = 0; s1[i] != '\0'; i++)
+	for (i = 0; s1 && (s1[i] != '\0'); i++)
 		len++;
-	for (i = 0; s2[i] != '\0'; i++)
+	for (i = 0; s2 && (s2[i] != '\0'); i++)
 		len++;
 	total = ++len;
 	concat = malloc(sizeof(char) * total);
 	if (concat == NULL)
 		return (NULL);
-	for (i = 0; s1[i] != '\0'; i++)
+	for (i = 0; s1 && (s1[i] != '\0'); i++)
 		concat[i] = s1[i];
 	j = i;
-	for (i = 0; (s2[i] != '\0') && (i < n); i++)
+	for (i = 0; s2 && (s2[i] != '\0') && (i < n); i++)
 		concat[j++] = s2[i];
 	concat[j] = '\0';
 	return (concat);
