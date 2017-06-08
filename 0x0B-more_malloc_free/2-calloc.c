22e8362814ddc5479dee7af341101a42f20ef8ef
Kiren Srinivasan <srinitude@gmail.com>
2017-06-08 21:59:06 +0000

    Adds logical fix to sizeof in 'calloc' C program
    sizeof(size) is the type's size, not the value of 'size'

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -17,7 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if ((nmemb == 0) || (size == 0))
 		return (NULL);
-	arr = malloc(sizeof(size) * nmemb);
+	arr = malloc(size * nmemb);
 	if (arr == NULL)
 		return (NULL);
 	for (i = 0; i < nmemb; i++)
