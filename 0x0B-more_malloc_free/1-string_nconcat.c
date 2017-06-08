dac20d2425c8853ff68efb6595a22c31742b7bbe
Kiren Srinivasan <srinitude@gmail.com>
2017-06-08 19:37:12 +0000

    Moves 'j' index to for loop 'after' spot in 'string_nconcat' C program

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -28,8 +28,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; s1 && (s1[i] != '\0'); i++)
 		concat[i] = s1[i];
 	j = i;
-	for (i = 0; s2 && (s2[i] != '\0') && (i < n); i++)
-		concat[j++] = s2[i];
+	for (i = 0; s2 && (s2[i] != '\0') && (i < n); i++, j++)
+		concat[j] = s2[i];
 	concat[j] = '\0';
 	return (concat);
 }
