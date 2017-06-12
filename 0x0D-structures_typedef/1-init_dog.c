e058d0b9270661c497da6191c957cf469f5f8e31
Kiren Srinivasan <srinitude@gmail.com>
2017-06-12 23:44:36 +0000

    Adds NULL ptr cases for name and owner in 'init_dog' C program

--- 0x0D-structures_typedef/1-init_dog.c
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "dog.h"
 
 /**
@@ -11,7 +12,9 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
+	if (name != NULL)
+		d->name = name;
 	d->age = age;
-	d->owner = owner;
+	if (owner != NULL)
+		d->owner = owner;
 }
