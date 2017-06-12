38717f168ae6485135c8c2f00aca89eaa26c3d01
Kiren Srinivasan <srinitude@gmail.com>
2017-06-12 23:18:30 +0000

    Adds initial 'dog struct' C program

---
+++ 0x0D-structures_typedef/README.md
@@ -0,0 +1,3 @@
+# 0x0D. C - Structures, typedef
+
+Here's my project related to structures and typedef
---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - Representation of a dog
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ *
+ * Description: Here's our initial model of how we want to represent
+ * a dog. This may expand or contract later on.
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG */
