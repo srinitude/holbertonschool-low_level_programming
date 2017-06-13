1c01d74fa2f1f1d39098bddf9c271910e962b81e
Kiren Srinivasan <srinitude@gmail.com>
2017-06-13 00:30:34 +0000

    Adds initial 'create new dog' C program

---
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * _strdup - Duplicates a string
+ * @str: The string in question
+ *
+ * Return: Pointer to the new string
+ */
+
+char *_strdup(char *str)
+{
+	char *ptr;
+	int i;
+	int len = 0;
+
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+		len++;
+	ptr = malloc(++len * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+		ptr[i] = str[i];
+	ptr[i] = '\0';
+	return (ptr);
+}
+
+/**
+ * new_dog - Creates a new dog on the heap
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ * Return: Pointer to the new dog or NULL
+ *
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *doggie;
+
+	doggie = malloc(sizeof(dog_t));
+	if (doggie == NULL)
+		return (NULL);
+	if (name != NULL)
+		doggie->name = _strdup(name);
+	doggie->age = age;
+	if (owner != NULL)
+		doggie->owner = _strdup(owner);
+	return (doggie);
+}
