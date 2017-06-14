1aea7317c232bf830fbb08865c74cf01b7fb2c6c
Kiren Srinivasan <srinitude@gmail.com>
2017-06-14 20:37:40 +0000

    Adds initial 'int_index' C program

---
+++ 0x0E-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - Index where cmp doesn't return 0
+ * @array: Array to comb through
+ * @size: Size of the array
+ * @cmp: Comparison function
+ *
+ * Return: Index or -1
+ *
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, result;
+
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		result = cmp(array[i]);
+		if (result != 0)
+			return (i);
+	}
+	return (-1);
+}
