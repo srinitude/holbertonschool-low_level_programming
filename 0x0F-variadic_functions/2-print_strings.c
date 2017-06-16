839d439c712e00332292137eea34775fb4570a25
Kiren Srinivasan <srinitude@gmail.com>
2017-06-16 20:50:47 +0000

    Adds initial 'print numbers with var args' C program

---
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -0,0 +1,35 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - Prints strings in variadic
+ * @separator: The separator to print between strings
+ * @n: The number of arguments in variadic
+ *
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	const char *sep;
+	char *current;
+	va_list args;
+
+	if (separator == NULL)
+		sep = "";
+	else
+		sep = separator;
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		current = va_arg(args, char*);
+		if (current == NULL)
+			printf("(nil)");
+		else
+			printf("%s", current);
+		if (i < (n - 1))
+			printf("%s", sep);
+		else
+			printf("\n");
+	}
+	va_end(args);
+}
