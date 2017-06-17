be7cc6526f6ba84b1e33b646e66b7d26c887b6c2
Kiren Srinivasan <srinitude@gmail.com>
2017-06-17 19:00:23 +0000

    New fix to unknown betty probs in 'sum variadic func' C program

--- 0x0F-variadic_functions/0-sum_them_all.c
+++ 0x0F-variadic_functions/0-sum_them_all.c
@@ -15,7 +15,7 @@ int sum_them_all(const unsigned int n, ...)
 	va_list args;
 
 	if (n == 0)
-		return (sum);
+		return (0);
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 		sum += va_arg(args, unsigned int);
