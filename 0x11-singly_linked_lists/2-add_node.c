385e3d8912483677e09c0834ec0194f4e5054917
Kiren Srinivasan <srinitude@gmail.com>
2017-06-22 23:00:13 +0000

    Adds NULL checking in 'add node' C program

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -1,6 +1,25 @@
 #include "lists.h"
 
 /**
+ * _strlen - Computes length of string
+ * @str: The string in question
+ *
+ * Return: The length of the string or 0
+ *
+ */
+
+int _strlen(const char *str)
+{
+	int i;
+
+	if (str == NULL)
+		return (0);
+	for (i = 0; str[i]; i++)
+		;
+	return (i);
+}
+
+/**
  * create_node - Creates a new node for list
  * @str: The string to add to new node
  *
@@ -12,11 +31,13 @@ list_t *create_node(const char *str)
 {
 	list_t *new_node;
 
+	if (str == NULL)
+		return (NULL);
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->str = strdup(str);
-	new_node->len = strlen(str);
+	new_node->len = _strlen(str);
 	return (new_node);
 }
 
@@ -31,9 +52,13 @@ list_t *create_node(const char *str)
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t **current_head = head;
-	list_t *new_head = create_node(str);
+	list_t **current_head;
+	list_t *new_head;
 
+	if (head == NULL || str == NULL)
+		return (NULL);
+	current_head = head;
+	new_head = create_node(str);
 	if (new_head == NULL)
 		return (new_head);
 	new_head->next = *current_head;
