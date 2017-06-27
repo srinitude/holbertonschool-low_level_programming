aeac34efb9d66120339c34aa675786ce1946aba2
Kiren Srinivasan <srinitude@gmail.com>
2017-06-27 19:32:13 +0000

    Adds 'NULL' check for head dbl ptr in 'insert node at index' C program

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -65,12 +65,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *next_node;
 	listint_t *current_head;
 
+	if (head == NULL)
+		return (NULL);
 	new_node = create_new_node(n);
 	if (new_node == NULL)
 		return (NULL);
 	if (idx == 0)
 	{
-		if (head == NULL)
+		if (*head == NULL)
 			*head = new_node;
 		else
 		{
