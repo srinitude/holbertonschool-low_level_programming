d09e39496a743ce5defb341caf7719b1a9e746b3
Kiren Srinivasan <srinitude@gmail.com>
2017-06-30 21:36:01 +0000

    Adds initial 'determine # of bit flips' C program

---
+++ 0x13-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * flip_bits - Determine number of bit flips
+ * @n: First number
+ * @m: Second number
+ *
+ * Return: Number of flips
+ *
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int i, flips = 0, num_bits;
+	unsigned long int flip_mask, looper;
+
+	flip_mask = n ^ m;
+	num_bits = sizeof(unsigned long int) * 8 - 1;
+	looper = ~0;
+	looper = looper & 1;
+	for (i = 0; i < num_bits; i++)
+	{
+		if ((flip_mask & looper) > 0)
+			flips++;
+		looper = looper << 1;
+	}
+	return (flips);
+}
