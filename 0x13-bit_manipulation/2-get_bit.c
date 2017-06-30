a5ace2ccaa97f883d0b1b0944e23a071614c6669
Kiren Srinivasan <srinitude@gmail.com>
2017-06-30 00:47:32 +0000

    Adds initial 'get bit' C program

---
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+
+/**
+ * two_much_power - Exponentation with base 2
+ * @degree: The degree to raise base to
+ *
+ * Return: Result
+ *
+ */
+
+unsigned int two_much_power(unsigned int degree)
+{
+	unsigned int i;
+	unsigned int result = 1;
+
+	for (i = 0; i < degree; i++)
+		result *= 2;
+	return (result);
+}
+
+/**
+ * get_bit - Gets the value of a bit at given index
+ * @n: The number to retrieve bit from
+ * @index: The index to retrieve
+ *
+ * Return: The index or -1
+ *
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int mask, result;
+
+	mask = two_much_power(index);
+	if (mask > n)
+		return (-1);
+	result = n & mask;
+	if (result == mask)
+		return (1);
+	return (0);
+}
