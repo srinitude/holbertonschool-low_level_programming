ad13230c6a6a567ade5980aad5c92c69daea33d2
Kiren Srinivasan <srinitude@gmail.com>
2017-07-03 20:21:45 +0000

    Closes file descriptors when syscall fails in 'read and print file' C program

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -21,13 +21,22 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
+	{
+		close(fd);
 		return (0);
+	}
 	chars_read = read(fd, buffer, letters);
 	if (chars_read == -1)
+	{
+		close(fd);
 		return (0);
+	}
 	chars_write = write(1, buffer, chars_read);
 	if ((chars_write == -1) || (chars_write != chars_read))
+	{
+		close(fd);
 		return (0);
+	}
 	close(fd);
 	free(buffer);
 	return (chars_write);
