de0fc3032d6a9a20c2bf37264947774bfe4ab3bb
Kiren Srinivasan <srinitude@gmail.com>
2017-07-03 22:13:33 +0000

    Does not exit early if text_content is NULL in 'create file' C program

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -38,15 +38,13 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
-	if (text_content == NULL)
+	if (text_content)
 	{
-		close(fd);
-		return (1);
+		len = _strlen(text_content);
+		write_result = write(fd, text_content, len);
+		if (write_result == -1)
+			return (write_result);
 	}
-	len = _strlen(text_content);
-	write_result = write(fd, text_content, len);
 	close(fd);
-	if (write_result == -1)
-		return (write_result);
 	return (1);
 }
