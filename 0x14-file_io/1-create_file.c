3e9b0a5c27512b70a6a9257b58bf17f82908aecd
Kiren Srinivasan <srinitude@gmail.com>
2017-07-03 22:08:14 +0000

    Adds read/write permissions to 'create file' C program

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -20,22 +20,6 @@ size_t _strlen(char *str)
 }
 
 /**
- * _strcpy - Copies string into buffer
- * @buf: The buffer to copy the string into
- * @str: The string to copy
- *
- */
-
-void _strcpy(char *buf, char *str)
-{
-	unsigned int i;
-
-	for (i = 0; str[i]; i++)
-		buf[i] = str[i];
-	buf[i] = '\0';
-}
-
-/**
  * create_file - Creates or truncates a file
  * @filename: Name of the file to create/truncate
  * @text_content: Content to add into the file
@@ -48,11 +32,10 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, write_result;
 	size_t len;
-	char *buffer;
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 	if (text_content == NULL)
@@ -61,13 +44,8 @@ int create_file(const char *filename, char *text_content)
 		return (1);
 	}
 	len = _strlen(text_content);
-	buffer = malloc(sizeof(char) * len);
-	if (buffer == NULL)
-		return (-1);
-	_strcpy(buffer, text_content);
-	write_result = write(fd, buffer, len);
+	write_result = write(fd, text_content, len);
 	close(fd);
-	free(buffer);
 	if (write_result == -1)
 		return (write_result);
 	return (1);
