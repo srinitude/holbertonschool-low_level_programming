3ebff0aebb171c5d25a4029db3e4248e2c952daf
Kiren Srinivasan <srinitude@gmail.com>
2017-08-04 19:55:07 +0000

    Adds initial 'add node to head of linked list' C program

---
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,51 @@
+#include "lists.h"
+
+/**
+ * create_node - Creates a new node
+ * @n: The data to instantiate with
+ * @prev: Reference to previous node
+ * @next: Reference to next node
+ *
+ * Return: Address of the new node or NULL
+ */
+dlistint_t *create_node(int n, dlistint_t *prev, dlistint_t *next)
+{
+	dlistint_t *node;
+
+	node = malloc(sizeof(dlistint_t));
+	if (node == NULL)
+		return (NULL);
+	node->n = n;
+	node->prev = prev;
+	node->next = next;
+
+	return (node);
+}
+
+/**
+ * add_dnodeint - Adds node to head of linked list
+ * @head: Double pointer to head of linked list
+ * @n: The number to instantiate the new head with
+ *
+ * Return: Address of the new head
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node = NULL, *current_head = NULL;
+
+	if (head == NULL)
+		return (NULL);
+	new_node = create_node(n, NULL, NULL);
+	if (new_node == NULL)
+		return (NULL);
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (*head);
+	}
+	current_head = *head;
+	current_head->prev = new_node;
+	new_node->next = current_head;
+	*head = new_node;
+	return (*head);
+}
