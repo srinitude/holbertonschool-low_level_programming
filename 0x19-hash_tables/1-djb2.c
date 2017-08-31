e32880a923e587a64649e8744ae71e03b0ece1a5
Kiren Srinivasan <srinitude@gmail.com>
2017-08-31 20:26:19 +0000

    Adds initial djb2 hashing function

---
+++ 0x19-hash_tables/1-djb2.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - djb2 hash function
+ * @str: The key to hash
+ *
+ * Return: The hash of the key
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+	while ((c = *str++))
+		hash = ((hash << 5) + hash) + c;
+	return (hash);
+}
