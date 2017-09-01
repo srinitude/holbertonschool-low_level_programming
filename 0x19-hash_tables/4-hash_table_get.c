188132a6c3ee7a22f1412f96b8e6ebd932b51558
Kiren Srinivasan <srinitude@gmail.com>
2017-09-01 17:26:23 +0000

    Checks for NULL hash table and key in 'get value for key' C function

--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -12,6 +12,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *node = NULL;
 	unsigned long int index;
 
+	if (!ht || !key)
+		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
 	node = ht->array[index];
 	while (node)
