b2b422c56cf0025f40dc4aa46e4b115ef604e489
Kiren Srinivasan <srinitude@gmail.com>
2017-09-14 18:28:40 +0000

    Adds initial header file for sorting algo project

---
+++ 0x1A-sorting_algorithms/sort.h
@@ -0,0 +1,23 @@
+#ifndef SORTING_ALGOS
+#define SORTING_ALGOS
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+#endif /* SORTING_ALGOS */
