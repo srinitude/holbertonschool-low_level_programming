e48df93669ff06af731b8653def412c89e506bed
Kiren Srinivasan <srinitude@gmail.com>
2017-10-23 19:00:21 +0000

    Creates a new binary tree node

---
+++ 0x1C-binary_trees/0-binary_tree_node.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - Creates a binary tree node
+ * @parent: The parent node
+ * @value: The value of the node
+ *
+ * Return: A new node or NULL
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	new_node = malloc(sizeof(binary_tree_t));
+	if (!new_node)
+		return (NULL);
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	new_node->n = value;
+	return (new_node);
+}
--- 0x1C-binary_trees/binary_trees.h
+++ 0x1C-binary_trees/binary_trees.h
@@ -29,4 +29,6 @@ typedef struct binary_tree_s heap_t;
 
 void binary_tree_print(const binary_tree_t *);
 
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
 #endif /* BINARY_TREE */
