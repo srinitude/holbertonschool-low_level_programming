8945838014e3dc8af7af7b6dffce9895c73776f2
Kiren Srinivasan <srinitude@gmail.com>
2017-10-24 00:03:08 +0000

    Adds formatting fixes

--- 0x1C-binary_trees/8-binary_tree_postorder.c
+++ 0x1C-binary_trees/8-binary_tree_postorder.c
@@ -13,7 +13,7 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 		return;
 	if (!func)
 		return;
-	binary_tree_postorder(tree->left, func); 
+	binary_tree_postorder(tree->left, func);
 	binary_tree_postorder(tree->right, func);
 	func(tree->n);
 }
--- 0x1C-binary_trees/9-binary_tree_height.c
+++ 0x1C-binary_trees/9-binary_tree_height.c
@@ -10,7 +10,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	binary_tree_t *right = NULL, *left = NULL;
 	size_t right_height = 0, left_height = 0;
-	
+
 	if (!tree)
 		return (0);
 	if (!tree->left && !tree->right)
