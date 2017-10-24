12af749111ea613034c1f9591bc24861138557ad
Kiren Srinivasan <srinitude@gmail.com>
2017-10-24 01:45:33 +0000

    Adds initial tree balance program

--- 0x1C-binary_trees/14-binary_tree_balance.c
+++ 0x1C-binary_trees/14-binary_tree_balance.c
@@ -23,3 +23,26 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (1 + right_height);
 	return (1 + left_height);
 }
+
+/**
+ * binary_tree_balance - Finds the balance of a binary tree
+ * @tree: The tree in question
+ *
+ * Return: The difference
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	binary_tree_t *left = NULL, *right = NULL;
+	int diff = 0, left_height = 0, right_height = 0;
+
+	if (!tree)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (0);
+	left = tree->left;
+	right = tree->right;
+	left_height = binary_tree_height(left);
+	right_height = binary_tree_height(right);
+	diff = left_height - right_height;
+	return (diff);
+}
