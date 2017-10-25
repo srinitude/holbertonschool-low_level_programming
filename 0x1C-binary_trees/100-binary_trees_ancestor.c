1313948369427380b8ea42d312a4df507f14f519
Kiren Srinivasan <srinitude@gmail.com>
2017-10-25 22:52:17 +0000

    Makes C files Betty compliant

--- 0x1C-binary_trees/100-binary_trees_ancestor.c
+++ 0x1C-binary_trees/100-binary_trees_ancestor.c
@@ -1,6 +1,7 @@
 #include "binary_trees.h"
 
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t
+*binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
 {
 	if (!first || !second)
 		return (NULL);
