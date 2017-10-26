03b99f19fb0a39529c838eaf569735f456991fbe
Kiren Srinivasan <srinitude@gmail.com>
2017-10-26 22:05:28 +0000

    Adds initial nearest ancestor function

--- 0x1C-binary_trees/100-binary_trees_ancestor.c
+++ 0x1C-binary_trees/100-binary_trees_ancestor.c
@@ -1,9 +1,19 @@
 #include "binary_trees.h"
 
+binary_tree_t *ancestor(binary_tree_t *f, binary_tree_t *s, size_t c)
+{
+	if (!f || !s)
+		return (NULL);
+	if (f == s)
+		return (f);
+	if (c % 2)
+		return (ancestor(f->parent, s, c + 1));
+	else
+		return (ancestor(f, s->parent, c + 1));
+}
+
 binary_tree_t
 *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
 {
-	if (!first || !second)
-		return (NULL);
-	
+	return (ancestor((binary_tree_t *)first, (binary_tree_t *)second, 0));
 }
