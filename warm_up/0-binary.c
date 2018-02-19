42371046476f0fa6bc5725641f7f0f115914a54d
Kiren Srinivasan <srinitude@gmail.com>
2018-02-19 18:08:41 +0000

    Adds initial binary search implemenation

---
+++ warm_up/0-binary.c
@@ -0,0 +1,55 @@
+#include "search_algos.h"
+
+/**
+ * binary_search_imp - Implementation of binary search
+ * @array: The array to search through
+ * @val: The value to search for
+ * @l: Lower bound to start printing
+ * @u: Upper bound to end printing
+ *
+ * Return: The index of the value or -1 if the value isn't present/array is NULL
+ */
+int binary_search_imp(int *array, int value, size_t l, size_t u)
+{
+	size_t mid, i;
+	int first = 1;
+
+	printf("Searching in array: ");
+	for (i = l; i <= u; i++)
+	{
+		if (first)
+		{
+			printf("%i", array[i]);
+			first = 0;
+		}
+		else
+			printf(", %i", array[i]);
+	}
+	printf("\n");
+
+	mid = (l + u) / 2;
+	if (l == u && array[mid] != value)
+		return (-1);
+	if (array[mid] == value)
+		return ((int)mid);
+
+	if (array[mid] > value)
+		return (binary_search_imp(array, value, l, mid - 1));
+	else
+		return (binary_search_imp(array, value, mid + 1, u));
+}
+
+/**
+ * binary_search - Search for a value in an array using binary search
+ * @array: The array to search through
+ * @size: The size of the array
+ * @value: The value to search for
+ *
+ * Return: The index of the value or -1 if the value isn't present/array is NULL
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	if (array == NULL)
+		return (-1);
+	return binary_search_imp(array, value, (size_t)0, size - 1);
+}
---
+++ warm_up/search_algos.h
@@ -0,0 +1,9 @@
+#ifndef WARM_UP
+#define WARM_UP
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int binary_search(int *array, size_t size, int value);
+
+#endif /* WARM_UP */
